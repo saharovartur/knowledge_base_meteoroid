63f98b67dd7c5a4c8a2d21ccba6d44b46b6afcba
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-16 15:05:31 +0400

    написал функцию вью для регистрации. Хешируем пароль в sey_password, при создании пользователя - создается профиль для него туда сразу вносятся данные о компании. Компания указывается та к которой принадлежит создатель.

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 
+from accounts.forms import UserRegistrationForm
 from accounts.models import Profile
 
 
@@ -22,5 +23,25 @@ def profile(request):
     return render(request, 'profile.html', {'profile_data': profile_data})
 
 
+
+
+@login_required
 def register(request):
-    pass
+    """Вью регистрации"""
+    user = request.user
+    company_request_user = user.profile.company
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+
+            Profile.objects.create(user=new_user, company=company_request_user)
+            context = {'new_user': new_user}
+            return render(request, 'accounts/register_done.html', context)
+
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'accounts/register.html', {'user_form': user_form})
+
