7fae0c534240ab4b9a173e3ea90a51b0bcdba66e
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-18 16:16:38 +0400

    добавил урл для обработки DeleteView для удаления объекта post, изменил название view для маршрута post_list

--- content/urls.py
+++ content/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from content import views
-from content.views import PostDetailView
+from content.views import PostDeleteView
 
 urlpatterns = [
-    path('', views.user_posts, name='post_list'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('', views.company_posts, name='post_list'),
+    path('post/<int:id>/', views.post_detail, name='post-detail'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('about/', views.about, name='about'),
 
 ]
