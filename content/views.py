32d92637cf77b90b499bc0d1cad367acd74f201d
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-18 16:18:12 +0400

    добавил связь с redis, добавил переменную для хранения увеличения количества просмотров

--- content/views.py
+++ content/views.py
@@ -1,14 +1,20 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DetailView
+import redis
+from django.conf import settings
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView
 
 from content.models import Post
-
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+# связь с redis
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
 
 
 @login_required
-def user_posts(request):
+def company_posts(request):
     current_user = request.user.profile
     current_company = current_user.company
     posts = Post.objects.filter(
@@ -17,11 +23,23 @@ def user_posts(request):
     return render(request, 'post_list.html', {'posts': posts})
 
 
-class PostDetailView(LoginRequiredMixin, DetailView):
+@login_required
+def post_detail(request, id):
+    """Представление детальной инфо поста"""
+    post = get_object_or_404(Post, pk=id)
+    total_views = r.incr(f'post:{post.id}:views')  # incr увеличивает значение на 1
+    return render(request, 'content/post_detail.html',
+                  {'post': post,
+                   'total_views': total_views})
+
+
+class PostDeleteView(DeleteView):
     model = Post
-    template_name = 'post_detail.html'
+    success_url = reverse_lazy('post_list')
 
 
 def about(request):
     return render(request, 'about.html')
+
+
 
